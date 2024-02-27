cca352a547c2e8107030812799bd6bb830d807ef
Oliver Quarm <ollie@nakatomi.com>
2024-02-27 20:57:47 +1100

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Ollie Quarm on 27/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
