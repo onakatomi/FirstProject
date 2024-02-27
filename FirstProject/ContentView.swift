f1adfb795dbd22f4978b7e19654a7e0883da8a19
Oliver Quarm <ollie@nakatomi.com>
2024-02-27 22:18:19 +1100

    refactor

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -21,19 +21,21 @@ struct ContentView: View {
 
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("✅").font(.largeTitle)
             } else {
                 RoundedRectangle(cornerRadius: 12)
             }
 
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
